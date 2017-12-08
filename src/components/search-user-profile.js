d77866c38e9bd2cdb0f83801466dc635d32ee288
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2017-12-08 15:55:50 -0800

    jacks friday commit

--- src/components/search-user-profile.js
+++ src/components/search-user-profile.js
@@ -4,17 +4,21 @@ import { connect } from 'react-redux';
 export class SearchUserProfile extends React.Component {
 
   render() {
+    if(this.props.firstName && this.props.lastName) {
+
+    }
+
     return (
       <div id="search-user-profile">
-        {/* <div className="search-user-profile-name"><h1>`{this.props.firstName} {this.props.lastName}`</h1></div>
-        <div className="search-user-profile-city">Name: {this.props.city}</div>
-        <div className="search-user-profile-state">Name: {this.props.state}</div>
-        <div className="search-user-profile-age">Name: {this.props.age}</div>
-        <div className="search-user-profile-bio">Name: {this.props.bio}</div>
-        <div className="search-user-profile-interests">Name: {this.props.interests}</div>
-        <div className="search-user-profile-music">Name: {this.props.music}</div>
-        <div className="search-user-profile-movies">Name: {this.props.movies}</div>
-        <div className="search-user-profile-tv">Name: {this.props.tv}</div> */}
+        <div className="search-user-profile-name"><h1>{this.props.firstName} {this.props.lastName}</h1></div>
+        <div className="search-user-profile-city">City: {this.props.city}</div>
+        <div className="search-user-profile-state">State: {this.props.state}</div>
+        <div className="search-user-profile-age">Age: {this.props.age}</div>
+        <div className="search-user-profile-bio">Bio: {this.props.bio}</div>
+        <div className="search-user-profile-interests">Interests: {this.props.interests}</div>
+        <div className="search-user-profile-music">Music: {this.props.music}</div>
+        <div className="search-user-profile-movies">Movies: {this.props.movies}</div>
+        <div className="search-user-profile-tv">TV: {this.props.tv}</div>
       </div>
     )
   }
@@ -22,18 +26,18 @@ export class SearchUserProfile extends React.Component {
 
 export const mapStateToProps = state => {
   console.log("THIS IS THE ONE", state.user.selectedUser)
-  {
-  // id: state.user.selectedUser.id,
-  // firstName: state.user.selectedUser.firstName,
-  // lastName: state.user.selectedUser.lastName,
-  // city: state.user.selectedUser.city,
-  // state: state.user.selectedUser.state,
-  // age: state.user.selectedUser.age,
-  // bio: state.user.selectedUser.bio,
-  // interests: state.user.selectedUser.interests,
-  // music: state.user.selectedUser.music,
-  // movies: state.user.selectedUser.movies,
-  // tv: state.user.selectedUser.tv
+  return {
+  id: state.user.selectedUser.id,
+  firstName: state.user.selectedUser.firstName,
+  lastName: state.user.selectedUser.lastName,
+  city: state.user.selectedUser.city,
+  state: state.user.selectedUser.state,
+  age: state.user.selectedUser.age,
+  bio: state.user.selectedUser.bio,
+  interests: state.user.selectedUser.interests,
+  music: state.user.selectedUser.music,
+  movies: state.user.selectedUser.movies,
+  tv: state.user.selectedUser.tv
 }};
 
 export default connect(mapStateToProps)(SearchUserProfile);
