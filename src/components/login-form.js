3f79fdb2a19edf60a369d444e4634ff9a3f07367
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2017-12-07 18:39:11 -0800

    navigation improved

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -27,32 +27,37 @@ export class LoginForm extends React.Component {
         }
 
         return (
-            <form
-                className="login-form"
-                onSubmit={this.props.handleSubmit(values =>
-                    this.onSubmit(values)
-                )}>
-                {error}
-                <label htmlFor="username">Username</label>
-                <Field
-                    component={Input}
-                    type="text"
-                    name="username"
-                    id="username"
-                    validate={[required, nonEmpty]}
-                />
-                <label htmlFor="password">Password</label>
-                <Field
-                    component={Input}
-                    type="password"
-                    name="password"
-                    id="password"
-                    validate={[required, nonEmpty]}
-                />
-                <button disabled={this.props.pristine || this.props.submitting}>
-                    Log in
-                </button>
-            </form>
+            <div>
+                <form
+                    className="login-form"
+                    onSubmit={this.props.handleSubmit(values =>
+                        this.onSubmit(values)
+                    )}>
+                    {error}
+                    <label htmlFor="username">Username</label>
+                    <Field
+                        component={Input}
+                        type="text"
+                        name="username"
+                        id="username"
+                        validate={[required, nonEmpty]}
+                    />
+                    <label htmlFor="password">Password</label>
+                    <Field
+                        component={Input}
+                        type="password"
+                        name="password"
+                        id="password"
+                        validate={[required, nonEmpty]}
+                    />
+                    <button disabled={this.props.pristine || this.props.submitting}>
+                        Log in
+                    </button>
+                    
+                </form>
+                <Link to="/">Back</Link>
+                <Link to="/searchPage">Create Account</Link>
+            </div>
         );
     }
 }
