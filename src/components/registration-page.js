d49558f4c594fb8912e1768a2f13d54a8289834a
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2017-12-07 20:09:51 -0800

    already a member space added

--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -14,7 +14,7 @@ export function RegistrationPage(props) {
         <div className="home">
             <h2>Register for Roommate Finder</h2>
             <RegistrationForm />
-            <span className="login-text">Already a member?</span>
+            <span className="login-text">Already a member? </span>
             <span className="login-link"><Link to="/login">Login</Link></span>
         </div>
     );
