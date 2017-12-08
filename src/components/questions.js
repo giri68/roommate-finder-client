151ed408d555d5e93015a5778a941955c2f4c375
ericpcho <ericpcho@gmail.com>
2017-12-08 00:07:48 -0800

    changed the names to match the server side data in questions.js

--- src/components/questions.js
+++ src/components/questions.js
@@ -10,12 +10,14 @@ import {saveQuestions} from '../actions/user';
 export class Questions extends React.Component {
 
   onSubmit(values) {
-    const {firstName, lastName, age, gender, city, state, searchRadius, 
-      maxPrice, pets, pets2, music, music2, cigarettes, cigarettes2, alcohol, alcohol2, marijuana, 
-      marijuana2, sleep, sleep2, guests, guests2, clean, clean2} = values;
-    const user = {firstName, lastName, age, gender, city, state, searchRadius, 
-      maxPrice, pets, pets2, music, music2, cigarettes, cigarettes2, alcohol, alcohol2, marijuana, 
-      marijuana2, sleep, sleep2, guests, guests2, clean, clean2};
+    const {firstName, lastName, age, gender, gender_bothered, city, state, max_distance, 
+      max_price, pets_have, pets_bothered, loud_music, loud_music_bothered, cigarettes, cigarettes_bothered, 
+      drinking_day_per_week, drinking_bothered, alt_smoking, alt_smoking_bothered, hour_awake, hours_bothered,
+      guests_frequency, guests_bothered, cleanliness, cleanliness_bothered} = values;
+    const user = {firstName, lastName, age, gender, gender_bothered, city, state, max_distance, 
+      max_price, pets_have, pets_bothered, loud_music, loud_music_bothered, cigarettes, cigarettes_bothered, 
+      drinking_day_per_week, drinking_bothered, alt_smoking, alt_smoking_bothered, hour_awake, hours_bothered,
+      guests_frequency, guests_bothered, cleanliness, cleanliness_bothered};
       user.username = this.props.currentUser.username
       console.log(user)
     return this.props.dispatch(saveQuestions(user))
@@ -77,47 +79,47 @@ export class Questions extends React.Component {
               name="state"
               validate={[required, nonEmpty, isTrimmed]}
           />
-          <label htmlFor="searchRadius">Search Radius(miles)</label>
+          <label htmlFor="max_distance">Search Radius(miles)</label>
           <Field
               component={Input}
               type="number"
-              name="searchRadius"
+              name="max_distance"
               validate={[required, nonEmpty, minValue1]}
           />
-           <label htmlFor="maxPrice">Maximum Price</label>
+           <label htmlFor="max_price">Maximum Price</label>
           <Field
               component={Input}
               type="number"
-              name="maxPrice"
+              name="max_price"
               validate={[required, nonEmpty, minValue100]}
           />
           <h1>Personality Profile</h1>
-           <label htmlFor="pets">Do you have pets?</label>
+           <label htmlFor="pets_have">Do you have pets?</label>
           <Field
               component={Input}
               type="text"
-              name="pets"
+              name="pets_have"
               validate={[required, nonEmpty]}
           />
-          <label htmlFor="pets2">How likely are you to live with someone who had pets?</label>
+          <label htmlFor="pets_bothered">How likely are you to live with someone who had pets?</label>
           <Field
               component={Input}
               type="number"
-              name="pets2"
+              name="pets_bothered"
               validate={[required, nonEmpty]}
           />
-          <label htmlFor="music">Do you listen to loud music in your living space?</label>
+          <label htmlFor="loud_music">Do you listen to loud music in your living space?</label>
           <Field
               component={Input}
               type="text"
-              name="music"
+              name="loud_music"
               validate={[required, nonEmpty]}
           />
-          <label htmlFor="music2">How likely are you to live with someone who listens to loud music?</label>
+          <label htmlFor="loud_music_bothered">How likely are you to live with someone who listens to loud music?</label>
           <Field
               component={Input}
               type="number"
-              name="music2"
+              name="loud_music_bothered"
               validate={[required, nonEmpty]}
           />
           <label htmlFor="cigarettes">Do you smoke cigarettes?</label>
@@ -127,81 +129,88 @@ export class Questions extends React.Component {
               name="cigarettes"
               validate={[required, nonEmpty]}
           />
-          <label htmlFor="cigarettes2">How likely are you to live with someone who smokes cigarettes?</label>
+          <label htmlFor="cigarettes_bothered">How likely are you to live with someone who smokes cigarettes?</label>
           <Field
               component={Input}
               type="number"
-              name="cigarettes2"
+              name="cigarettes_bothered"
               validate={[required, nonEmpty]}
           />
-          <label htmlFor="alcohol">Do you consume alcohol?</label>
+          <label htmlFor="drinking_day_per_week">Do you consume alcohol?</label>
           <Field
               component={Input}
               type="text"
-              name="alcohol"
+              name="drinking_day_per_week"
               validate={[required, nonEmpty]}
           />
-          <label htmlFor="alcohol2">How likely are you to live with someone who consumes alcohol?</label>
+          <label htmlFor="drinking_bothered">How likely are you to live with someone who consumes alcohol?</label>
           <Field
               component={Input}
               type="number"
-              name="alcohol2"
+              name="drinking_bothered"
               validate={[required, nonEmpty]}
           />
-          <label htmlFor="marijuana">Do you smoke marijuana?</label>
+          <label htmlFor="alt_smoking">Do you smoke marijuana?</label>
           <Field
               component={Input}
               type="text"
-              name="marijuana"
+              name="alt_smoking"
               validate={[required, nonEmpty]}
           />
-          <label htmlFor="marijuana2">How likely are you to live with someone who smokes marijuana?</label>
+          <label htmlFor="alt_smoking_bothered">How likely are you to live with someone who smokes marijuana?</label>
           <Field
               component={Input}
               type="number"
-              name="marijuana2"
+              name="alt_smoking_bothered"
               validate={[required, nonEmpty]}
           />
-                    <label htmlFor="sleep">Are you up at odd hours of the night/morning?</label>
+                    <label htmlFor="hour_awake">Are you up at odd hours of the night/morning?</label>
           <Field
               component={Input}
               type="text"
-              name="sleep"
+              name="hour_awake"
               validate={[required, nonEmpty]}
           />
-          <label htmlFor="sleep2">How likely are you to live with someone who is awake at odd hours of the day?</label>
+          <label htmlFor="hours_bothered">How likely are you to live with someone who is awake at odd hours of the day?</label>
           <Field
               component={Input}
               type="number"
-              name="sleep2"
+              name="hours_bothered"
               validate={[required, nonEmpty]}
           />
-          <label htmlFor="guests">Do you have guests over frequently, including, but not limited to significant others? </label>
+          <label htmlFor="guests_frequency">Do you have guests over frequently, including, but not limited to significant others? </label>
           <Field
               component={Input}
               type="text"
-              name="guests"
+              name="guests_frequency"
               validate={[required, nonEmpty]}
           />
-          <label htmlFor="guests2">How likely are you to live with someone who has guests over frequently?</label>
+          <label htmlFor="guests_bothered">How likely are you to live with someone who has guests over frequently?</label>
           <Field
               component={Input}
               type="number"
-              name="guests2"
+              name="guests_bothered"
               validate={[required, nonEmpty]}
           />
-          <label htmlFor="clean">On a scale from 1-5, how clean are you, 5 being very clean.  </label>
+          <label htmlFor="cleanliness">On a scale from 1-5, how clean are you, 5 being very clean.  </label>
           <Field
               component={Input}
               type="number"
-              name="clean"
+              name="cleanliness"
               validate={[required, nonEmpty]}
           />
-          <label htmlFor="clean2">How likely are you to live with someone who has guests over frequently?</label>
+          <label htmlFor="cleanliness_bothered">How likely are you to live with someone who has guests over frequently?</label>
           <Field
               component={Input}
               type="number"
-              name="clean2"
+              name="cleanliness_bothered"
+              validate={[required, nonEmpty]}
+          />
+          <label htmlFor="gender_bothered">How likely are you to live with someone of the opposite sex?</label>
+          <Field
+              component={Input}
+              type="number"
+              name="gender_bothered"
               validate={[required, nonEmpty]}
           />
           <button
