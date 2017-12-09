06f7b249c9439ec258b2999e70ce532e1f65b81d
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2017-12-08 16:48:35 -0800

    search-user-profile updated to validate content

--- src/components/search-user-profile.js
+++ src/components/search-user-profile.js
@@ -4,21 +4,55 @@ import { connect } from 'react-redux';
 export class SearchUserProfile extends React.Component {
 
   render() {
-    if(this.props.firstName && this.props.lastName) {
 
+    let name; 
+    if(this.props.firstName && this.props.lastName){
+      name = <div className="search-user-profile-name"><h1>{this.props.firstName} {this.props.lastName}</h1></div>
+    }
+    let city; 
+    if(this.props.city){
+      city = <div className="search-user-profile-city">City: {this.props.city}</div>
+    }
+    let state; 
+    if(this.props.state){
+      state =  <div className="search-user-profile-state">State: {this.props.state}</div>
+    }
+    let age; 
+    if(this.props.age){
+      age = <div className="search-user-profile-age">Age: {this.props.age}</div>
+    }
+    let bio; 
+    if(this.props.bio){
+      bio = <div className="search-user-profile-bio">Bio: {this.props.bio}</div>
+    }
+    let interests; 
+    if(this.props.interests){
+      interests = <div className="search-user-profile-interests">Interests: {this.props.interests}</div>
+    }
+    let music; 
+    if(this.props.music){
+      music = <div className="search-user-profile-music">Music: {this.props.music}</div>
+    }
+    let movies; 
+    if(this.props.movies){
+      movies = <div className="search-user-profile-movies">Movies: {this.props.movies}</div>
+    }
+    let tv; 
+    if(this.props.tv){
+      tv = <div className="search-user-profile-tv">TV: {this.props.tv}</div>
     }
 
     return (
       <div id="search-user-profile">
-        <div className="search-user-profile-name"><h1>{this.props.firstName} {this.props.lastName}</h1></div>
-        <div className="search-user-profile-city">City: {this.props.city}</div>
-        <div className="search-user-profile-state">State: {this.props.state}</div>
-        <div className="search-user-profile-age">Age: {this.props.age}</div>
-        <div className="search-user-profile-bio">Bio: {this.props.bio}</div>
-        <div className="search-user-profile-interests">Interests: {this.props.interests}</div>
-        <div className="search-user-profile-music">Music: {this.props.music}</div>
-        <div className="search-user-profile-movies">Movies: {this.props.movies}</div>
-        <div className="search-user-profile-tv">TV: {this.props.tv}</div>
+        { name }
+        { city }
+        { state }
+        { age }
+        { bio }
+        { interests }
+        { music }
+        { movies }
+        { tv }
       </div>
     )
   }
