bd2d1416d249c0cdf5a3ae859a9f934602dc871b
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2017-12-12 20:47:21 -0800

    styles for profile updated

--- src/components/search-user-profile.js
+++ src/components/search-user-profile.js
@@ -52,24 +52,26 @@ export class SearchUserProfile extends React.Component {
       <div className="search-user-profile">
         <div className="profile-picture">
         </div>
-        { name }
-        { city }
-        { state }
-        { age }
-        { bio }
-        { interests }
-        { music }
-        { movies }
-        { tv }
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem 
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa 
-          quae ab illo inventore veritatis et quasi architecto beatae vitae 
-          dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit 
-          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores 
-          eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam 
-          est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci 
-          velit, sed quia non numquam eius modi tempora incidunt ut labore 
-          et dolore magnam aliquam quaerat voluptatem.</p>
+        <div className="right-section">
+          { name }
+          { city }
+          { state }
+          { age }
+          { bio }
+          { interests }
+          { music }
+          { movies }
+          { tv }
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem 
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa 
+            quae ab illo inventore veritatis et quasi architecto beatae vitae 
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit 
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores 
+            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam 
+            est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci 
+            velit, sed quia non numquam eius modi tempora incidunt ut labore 
+            et dolore magnam aliquam quaerat voluptatem.</p>
+          </div>
       </div>
     )
   }
