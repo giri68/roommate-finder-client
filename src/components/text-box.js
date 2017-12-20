7577e4d936077bf0fd296b397e215eaa24004d69
ericpcho <ericpcho@gmail.com>
2017-12-20 14:22:43 -0800

    added a way for you to click on profiles from the google map

--- src/components/text-box.js
+++ src/components/text-box.js
@@ -1,13 +1,18 @@
 import React from "react"
 import {connect} from 'react-redux';
+import {getSelectedUser, setSelectedUserMatch} from '../actions/user'; 
 
 export class TextBox extends React.Component {
-  
+    handleMatchClick(username, score) {
+        this.props.dispatch(getSelectedUser(username))
+            .then(() => this.props.dispatch(setSelectedUserMatch(score)))
+    }
       render() {
-
           return (
-              <div className="textBox">
-  {this.props.profile.username}{this.props.profile.score}
+              <div onClick={event => this.handleMatchClick(this.props.profile.username, this.props.profile.score)} className="textBox">
+              {"Username:"}{this.props.profile.username}
+              <br/>
+              {"Score:"}{this.props.profile.score}
               </div>
           );
       }
