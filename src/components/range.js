e85b7a3f75d113c423580f32c898a84297d05aad
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2017-12-20 12:46:44 -0800

    questions updated

--- src/components/range.js
+++ src/components/range.js
@@ -1,8 +1,15 @@
 import React from 'react'; 
 
 
-export default function Range(props) {
-  return (
-    <input type="range" value={props.input.value} min="1" max="5" className="slider" onChange={props.input.onChange} />
-  )
+export default class Range extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return (
+      <input type="range" value={this.props.input.value} min="1" max="5" className="slider" onChange={this.props.input.onChange} />
+    )
+  }
+
 }
