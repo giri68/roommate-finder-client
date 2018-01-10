fffaa74a129411b96f112bcc26e88ac9fb72eb55
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-01-10 07:17:09 -0800

    mobile adjust added to landing page

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -2,22 +2,21 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Link, Redirect} from 'react-router-dom';
 import '../styles/landing-page.css'; 
-
 import LoginForm from './login-form';
 
 export default function LandingPage(props) {
 
     return (
-        <div className="landing-page">
-            <div className="text-section">
-                <h1>Roommate Finder</h1>
-                <p>Don't just look for a great apartment, look for a great roommate.</p>
-                <Link to="/start"><button className="button-blue">Get Started</button></Link>
-                <div>
-                <p>Already have an account? <span className="login-link"><Link to="/login">Login</Link></span></p>
+            <div className="landing-page">
+                <div className="text-section">
+                    <h1>Roommate Finder</h1>
+                    <p>Don't just look for a great apartment, look for a great roommate.</p>
+                    <Link to="/start"><button className="button-blue">Get Started</button></Link>
+                    <div>
+                    <p>Already have an account? <span className="login-link"><Link to="/login">Login</Link></span></p>
+                    </div>
                 </div>
             </div>
-        </div>
     );
 }
 
